06715dd29940eeb8ec3fd6d5b4652ac2d3b74d80
Kevin <xkevinmc@hotmail.com>
2017-02-11 00:21:00 +0100

    added favicon and cookie notification

--- MyWebSite/Models/EmailFormModel.cs
+++ MyWebSite/Models/EmailFormModel.cs
@@ -9,10 +9,15 @@ namespace MyWebSite.Models
     public class EmailFormModel
     {
         [Required, Display(Name="Your name")]
+        [StringLength(50, ErrorMessage = "Naam bevat te veel characters, max 50 characters toegestaan.")]
         public string FromName { get; set; }
+
         [Required, Display(Name = "Your email"), EmailAddress]
+        [StringLength(50, ErrorMessage = "E mail bevat te veel characters, max 50 characters toegestaan.")]
         public string FromEmail { get; set; }
+
         [Required, Display(Name = "Your message")]
+        [StringLength(500, ErrorMessage = "boodschap bevat te veel characters, max 500 characters toegestaan.")]
         public string Message { get; set; }
     }
 }
